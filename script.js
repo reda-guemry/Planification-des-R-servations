983d11d0921c033ee21a8d4a626313d8715199c6
reda guemry <guemryreda@gmail.com>
2025-10-29 22:35:54 +0100

    start at the popup

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+
+
 let buttonReserve = document.getElementById("buttonReserver") ;
+let popup = document.querySelector(".sectionform");
+
  
 
 
-
-const pupup = (let) => {
-    
-}
-document.addEventListener("click",(e))
+document.addEventListener("click" , function(e){
+    if(e.target.tagName === "BUTTON"){
+        popup.style.display = "flex";
+    }else if(e.target.tagName === "")
+})
