8765ec89d6ee289bdd97b3a0d0623ce2ba06ff14
reda guemry <guemryreda@gmail.com>
2025-10-29 23:15:53 +0100

    ajouter le ferme de pop up

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-
-
 let buttonReserve = document.getElementById("buttonReserver") ;
 let popup = document.querySelector(".sectionform");
-
  
 
 
 document.addEventListener("click" , function(e){
     if(e.target.tagName === "BUTTON"){
         popup.style.display = "flex";
-    }else if(e.target.tagName === "")
+    } else if(e.target.id === "close__pop"){
+        popup.style.display = "none";
+    }
+    
 })
+
